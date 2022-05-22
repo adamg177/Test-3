ef808a72e4310308bff03ae6902565d6cc9880b8
adamg177 <adamgrzesiak2@tlen.pl>
2022-05-22 20:38:19 +0200

    koniec dnia

--- Trello_frondend/frondend1/imports/ui/components/Register.vue
+++ Trello_frondend/frondend1/imports/ui/components/Register.vue
@@ -5,11 +5,11 @@
       <br><br><br>  
       <center><h1 class="h3 mb-3 fw-normal">Please register</h1></center>
 
-      <input   class="form-control" placeholder="Your Name or Nick" required>
+      <input v-model="username"  class="form-control" placeholder="Your Name or Nick" required>
 
-      <input  type="email" class="form-control" placeholder="Email" required>
+      <input v-model="email" type="email" class="form-control" placeholder="Email" required>
 
-      <input  type="password" class="form-control" placeholder="Password">
+      <input v-model="password" type="password" class="form-control" placeholder="Password">
 
       
       <button class="w-100 btn btn-lg btn-primary" type="submit">Sumbit</button>
@@ -19,6 +19,32 @@
 
 <script>
     export default{
-        name: "Register"
+        name: "Register",
+  setup() {
+    const data = reactive({
+      name: '',
+      email: '',
+      password: ''
+    });
+
+    //2 pobranie zwrotnych danych po rejestracji
+    const router = useRouter();
+
+    const submit = async () => {
+      console.log(data);
+      //wysylanie do backend na adres 8000 metody postrequest, headets z kontentem, wysylanie danych 
+      ///await fetch('http://localhost:3030/api/register', {
+      ///  method: 'POST',
+      /// headers: {'Content-Type': 'application/json'},
+      /// body: JSON.stringify(data)
+     /// });
+      //puszczenie danych na strone logowania
+      await router.push('/Login');
     }
- </script>
+    return {
+      data,
+      submit
+    }
+  }
+}
+</script>
