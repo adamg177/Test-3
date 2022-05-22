b5f59b1c014370ee76b67aa8b2e61a4e53e3ea5f
adamg177 <adamgrzesiak2@tlen.pl>
2022-05-22 20:02:08 +0200

    kod

--- Trello_frondend/frondend1/imports/ui/components/Register.vue
+++ Trello_frondend/frondend1/imports/ui/components/Register.vue
@@ -5,7 +5,7 @@
       <br><br><br>  
       <center><h1 class="h3 mb-3 fw-normal">Please register</h1></center>
 
-      <input  class="form-control" placeholder="Your Name or Nick" required>
+      <input   class="form-control" placeholder="Your Name or Nick" required>
 
       <input  type="email" class="form-control" placeholder="Email" required>
 
